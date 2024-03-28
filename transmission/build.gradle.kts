3a481ed78f9c9897e51c0cdd2610326ca3f70b64
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-03-28 09:26:35 +0300

    Update version

--- transmission/build.gradle.kts
+++ transmission/build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
 		create<MavenPublication>("release") {
 			groupId = "com.trendyol"
 			artifactId = "transmission"
-			version = "0.0.2"
+			version = "0.0.3"
 			afterEvaluate {
 				from(components["java"])
 			}
