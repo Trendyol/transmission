412508de5d0214829ff3f8567169ee19f7d52c3f
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-03-28 09:16:26 +0300

    Add top level builder functions for generic and typed handlers

--- transmission/src/main/java/com/trendyol/transmission/transformer/handler/EffectHandler.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/handler/EffectHandler.kt
@@ -5,3 +5,19 @@ import com.trendyol.transmission.Transmission
 fun interface EffectHandler {
 	suspend fun onEffect(effect: Transmission.Effect)
 }
+
+fun buildGenericEffectHandler(
+	onEffect: (effect: Transmission.Effect) -> Unit
+): EffectHandler {
+	return EffectHandler { effect -> onEffect(effect) }
+}
+
+inline fun <reified E : Transmission.Effect> buildTypedEffectHandler(
+	crossinline onEffect: suspend (effect: E) -> Unit
+): EffectHandler {
+	return EffectHandler { incomingEffect ->
+		incomingEffect
+			.takeIf { it is E }
+			?.let { effect -> onEffect(effect as E) }
+	}
+}
--- transmission/src/main/java/com/trendyol/transmission/transformer/handler/SignalHandler.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/handler/SignalHandler.kt
@@ -5,3 +5,19 @@ import com.trendyol.transmission.Transmission
 fun interface SignalHandler {
 	suspend fun onSignal(signal: Transmission.Signal)
 }
+
+fun buildGenericSignalHandler(
+	onSignal: suspend (signal: Transmission.Signal) -> Unit
+): SignalHandler {
+	return SignalHandler { signal -> onSignal(signal) }
+}
+
+inline fun <reified S : Transmission.Signal> buildTypedSignalHandler(
+	crossinline onSignal: suspend (signal: S) -> Unit
+): SignalHandler {
+	return SignalHandler { incomingSignal ->
+		incomingSignal
+			.takeIf { it is S }
+			?.let { signal -> onSignal(signal as S) }
+	}
+}
