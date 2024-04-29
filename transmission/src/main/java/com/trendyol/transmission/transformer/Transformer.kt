2037a2d32c56006d908dc7f0db30699f4045a979
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-04-29 10:57:16 +0300

    Add Transmission.Effect type as Generic parameter

--- transmission/src/main/java/com/trendyol/transmission/transformer/Transformer.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/Transformer.kt
@@ -17,25 +17,29 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-open class Transformer<D: Transmission.Data>(dispatcher: CoroutineDispatcher = Dispatchers.Default) {
+typealias DefaultTransformer = Transformer<Transmission.Data, Transmission.Effect>
+
+open class Transformer<D : Transmission.Data, E : Transmission.Effect>(
+	dispatcher: CoroutineDispatcher = Dispatchers.Default
+) {
 
 	private val dataChannel: Channel<D> = Channel(capacity = Channel.UNLIMITED)
-	private val effectChannel: Channel<Transmission.Effect> = Channel(capacity = Channel.UNLIMITED)
+	private val effectChannel: Channel<E> = Channel(capacity = Channel.UNLIMITED)
 
 	private val jobList: MutableList<Job?> = mutableListOf()
 
-	open val signalHandler: SignalHandler<D>? = null
+	open val signalHandler: SignalHandler<D, E>? = null
 
-	open val effectHandler: EffectHandler<D>? = null
+	open val effectHandler: EffectHandler<D, E>? = null
 
 	private val coroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
 
-	private val handlerScope: HandlerScope<D> = object : HandlerScope<D> {
+	private val handlerScope: HandlerScope<D, E> = object : HandlerScope<D, E> {
 		override fun publishData(data: D?) {
 			data?.let { dataChannel.trySend(it) }
 		}
 
-		override fun publishEffect(effect: Transmission.Effect) {
+		override fun publishEffect(effect: E) {
 			effectChannel.trySend(effect)
 		}
 	}
@@ -44,7 +48,7 @@ open class Transformer<D: Transmission.Data>(dispatcher: CoroutineDispatcher = D
 		incomingSignal: SharedFlow<Transmission.Signal>,
 		incomingEffect: SharedFlow<Transmission.Effect>,
 		outGoingData: SendChannel<D>,
-		outGoingEffect: SendChannel<Transmission.Effect>,
+		outGoingEffect: SendChannel<E>,
 	) {
 		jobList += coroutineScope.launch {
 			launch {
@@ -85,4 +89,28 @@ open class Transformer<D: Transmission.Data>(dispatcher: CoroutineDispatcher = D
 	fun clear() {
 		jobList.clearJobs()
 	}
+
+	// region handler extensions
+
+	inline fun <reified S : Transmission.Signal> Transformer<D, E>.buildTypedSignalHandler(
+		crossinline onSignal: suspend HandlerScope<D, E>.(signal: S) -> Unit
+	): SignalHandler<D, E> {
+		return SignalHandler { incomingSignal ->
+			incomingSignal
+				.takeIf { it is S }
+				?.let { signal -> onSignal(signal as S) }
+		}
+	}
+
+	inline fun <reified E : Transmission.Effect> Transformer<D, E>.buildTypedEffectHandler(
+		crossinline onEffect: suspend HandlerScope<D, E>.(effect: E) -> Unit
+	): EffectHandler<D, E> {
+		return EffectHandler { incomingEffect ->
+			incomingEffect
+				.takeIf { it is E }
+				?.let { effect -> onEffect(effect as E) }
+		}
+	}
+
+	// endregion
 }
--- transmission/src/main/java/com/trendyol/transmission/transformer/handler/SignalHandler.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/handler/SignalHandler.kt
@@ -3,22 +3,12 @@ package com.trendyol.transmission.transformer.handler
 import com.trendyol.transmission.Transmission
 import com.trendyol.transmission.transformer.Transformer
 
-fun interface SignalHandler<D: Transmission.Data> {
-	suspend fun HandlerScope<D>.onSignal(signal: Transmission.Signal)
+fun interface SignalHandler<D : Transmission.Data, E : Transmission.Effect> {
+	suspend fun HandlerScope<D, E>.onSignal(signal: Transmission.Signal)
 }
 
-fun<D: Transmission.Data> Transformer<D>.buildGenericSignalHandler(
-	onSignal: suspend HandlerScope<D>.(signal: Transmission.Signal) -> Unit
-): SignalHandler<D> {
+fun <D : Transmission.Data, E : Transmission.Effect> Transformer<D, E>.buildGenericSignalHandler(
+	onSignal: suspend HandlerScope<D, E>.(signal: Transmission.Signal) -> Unit
+): SignalHandler<D, E> {
 	return SignalHandler { signal -> onSignal(signal) }
-}
-
-inline fun <D: Transmission.Data, reified S : Transmission.Signal> buildTypedSignalHandler(
-	crossinline onSignal: suspend HandlerScope<D>.(signal: S) -> Unit
-): SignalHandler<D> {
-	return SignalHandler { incomingSignal ->
-		incomingSignal
-			.takeIf { it is S }
-			?.let { signal -> onSignal(signal as S) }
-	}
 }
