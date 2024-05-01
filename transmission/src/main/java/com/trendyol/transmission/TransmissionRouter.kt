56989ae886c15a5a24c95fbb12db2a01cafe83c4
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2024-05-01 23:21:08 +0300

    Allow multiple data holders to be defined and tracked inside Transformers

--- transmission/src/main/java/com/trendyol/transmission/TransmissionRouter.kt
+++ transmission/src/main/java/com/trendyol/transmission/TransmissionRouter.kt
@@ -21,90 +21,95 @@ import kotlinx.coroutines.withContext
 typealias DefaultTransmissionRouter = TransmissionRouter<Transmission.Data, Transmission.Effect>
 
 class TransmissionRouter<D : Transmission.Data, E : Transmission.Effect>(
-	private val transformerSet: Set<Transformer<D, E>>,
-	private val dispatcher: CoroutineDispatcher = Dispatchers.Default
+    private val transformerSet: Set<Transformer<D, E>>,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
-	private val coroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
+    private val coroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
 
-	private var initializationJob: Job? = null
+    private var initializationJob: Job? = null
 
-	// region signals
+    // region signals
 
-	private val _incomingSignals = Channel<Transmission.Signal>(capacity = Channel.UNLIMITED)
+    private val _incomingSignals = Channel<Transmission.Signal>(capacity = Channel.UNLIMITED)
 
-	private val sharedIncomingSignals = _incomingSignals.receiveAsFlow()
-		.shareIn(coroutineScope, SharingStarted.Lazily)
+    private val sharedIncomingSignals = _incomingSignals.receiveAsFlow()
+        .shareIn(coroutineScope, SharingStarted.Lazily)
 
-	fun processSignal(signal: Transmission.Signal) {
-		_incomingSignals.trySend(signal)
-	}
+    fun processSignal(signal: Transmission.Signal) {
+        _incomingSignals.trySend(signal)
+    }
 
-	// endregion
+    // endregion
 
-	// region effects
+    // region effects
 
-	private val effectChannel =
-		Channel<EffectWrapper<E, D, Transformer<D, E>>>(capacity = Channel.UNLIMITED)
+    private val effectChannel =
+        Channel<EffectWrapper<E, D, Transformer<D, E>>>(capacity = Channel.UNLIMITED)
 
-	private val sharedIncomingEffects = effectChannel.receiveAsFlow()
-		.shareIn(coroutineScope, SharingStarted.Lazily)
+    private val sharedIncomingEffects = effectChannel.receiveAsFlow()
+        .shareIn(coroutineScope, SharingStarted.Lazily)
 
-	// endregion
+    // endregion
 
-	private val outGoingDataChannel = Channel<D>(capacity = Channel.BUFFERED)
+    private val outGoingDataChannel = Channel<D>(capacity = Channel.BUFFERED)
 
-	// region data query
+    // region data query
 
-	private val outGoingQueryChannel: Channel<DataQuery> = Channel(capacity = Channel.BUFFERED)
+    private val outGoingQueryChannel: Channel<DataQuery> = Channel(capacity = Channel.BUFFERED)
 
-	private val queryResponseChannel: Channel<QueryResponse<D>> =
-		Channel(capacity = Channel.BUFFERED)
+    private val queryResponseChannel: Channel<QueryResponse<D>> =
+        Channel(capacity = Channel.BUFFERED)
 
-	private val incomingQueryResponse = queryResponseChannel.receiveAsFlow()
-		.shareIn(coroutineScope, SharingStarted.Lazily)
+    private val incomingQueryResponse = queryResponseChannel.receiveAsFlow()
+        .shareIn(coroutineScope, SharingStarted.Lazily)
 
-	private suspend fun processQuery(query: DataQuery) = withContext(dispatcher) {
-		val dataHolder = transformerSet
-			.filter { it.transmissionDataHolderState != HolderState.Undefined }
-			.find {
-				it.transmissionDataHolderState is HolderState.Initialized &&
-					(it.transmissionDataHolderState as HolderState.Initialized).value == query.type
-			}
-		queryResponseChannel.trySend(QueryResponse(query.sender, dataHolder?.holderData?.value))
-	}
+    private suspend fun processQuery(query: DataQuery) = withContext(dispatcher) {
+        val dataHolder = transformerSet
+            .filter { it.transmissionDataHolderState is HolderState.Initialized }
+            .find {
+                (it.transmissionDataHolderState as HolderState.Initialized)
+                    .valueSet.contains(query.type)
+            }
+        queryResponseChannel.trySend(
+            QueryResponse(
+                owner = query.sender,
+                data = dataHolder?.holderData?.value?.get(query.type)
+            )
+        )
+    }
 
-	// endregion
+    // endregion
 
-	fun initialize(
-		onData: ((D) -> Unit),
-		onEffect: (E) -> Unit = {},
-	) {
-		if (transformerSet.isEmpty()) {
-			throw IllegalStateException("transformerSet should not be empty")
-		}
-		initializationJob = coroutineScope.launch {
-			launch { sharedIncomingEffects.collect { onEffect(it.effect) } }
-			launch { outGoingQueryChannel.consumeAsFlow().collect { processQuery(it) } }
-			launch { outGoingDataChannel.consumeAsFlow().collect { onData(it) } }
-			transformerSet.map { transformer ->
-				launch {
-					transformer.initialize(
-						incomingSignal = sharedIncomingSignals,
-						incomingEffect = sharedIncomingEffects,
-						outGoingData = outGoingDataChannel,
-						outGoingEffect = effectChannel,
-						outGoingQuery = outGoingQueryChannel,
-						incomingQueryResponse = incomingQueryResponse
-					)
-				}
-			}
-		}
-	}
+    fun initialize(
+        onData: ((D) -> Unit),
+        onEffect: (E) -> Unit = {},
+    ) {
+        if (transformerSet.isEmpty()) {
+            throw IllegalStateException("transformerSet should not be empty")
+        }
+        initializationJob = coroutineScope.launch {
+            launch { sharedIncomingEffects.collect { onEffect(it.effect) } }
+            launch { outGoingQueryChannel.consumeAsFlow().collect { processQuery(it) } }
+            launch { outGoingDataChannel.consumeAsFlow().collect { onData(it) } }
+            transformerSet.map { transformer ->
+                launch {
+                    transformer.initialize(
+                        incomingSignal = sharedIncomingSignals,
+                        incomingEffect = sharedIncomingEffects,
+                        outGoingData = outGoingDataChannel,
+                        outGoingEffect = effectChannel,
+                        outGoingQuery = outGoingQueryChannel,
+                        incomingQueryResponse = incomingQueryResponse
+                    )
+                }
+            }
+        }
+    }
 
-	fun clear() {
-		initializationJob?.cancel()
-		transformerSet.forEach { it.clear() }
-	}
+    fun clear() {
+        initializationJob?.cancel()
+        transformerSet.forEach { it.clear() }
+    }
 
 }
--- transmission/src/main/java/com/trendyol/transmission/transformer/HolderState.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/HolderState.kt
@@ -2,5 +2,5 @@ package com.trendyol.transmission.transformer
 
 sealed interface HolderState {
 	data object Undefined: HolderState
-	data class Initialized(val value: String): HolderState
+	data class Initialized(val valueSet: Set<String>): HolderState
 }
