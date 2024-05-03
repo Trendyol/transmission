080851762090bf5ee6c68523a0299bc893607fbc
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-05-03 08:59:45 +0300

    Divide query into data and computation

--- transmission/src/main/java/com/trendyol/transmission/transformer/query/QueryResponse.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/query/QueryResponse.kt
@@ -2,4 +2,12 @@ package com.trendyol.transmission.transformer.query
 
 import com.trendyol.transmission.Transmission
 
-data class QueryResponse<D: Transmission.Data>(val owner: String, val data: D?)
+sealed class QueryResponse<D : Transmission.Data>(open val owner: String, open val data: D?) {
+	data class Data<D : Transmission.Data>(override val owner: String, override val data: D?) :
+		QueryResponse<D>(owner, data)
+
+	data class Computation<D : Transmission.Data>(
+		override val owner: String,
+		override val data: D?
+	) : QueryResponse<D>(owner, data)
+}
