62bdb08c42331cdc18f75b139d70cb242bde3c48
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-05-03 23:02:10 +0300

    Add documentation to RouterPayloadEffect

--- transmission/src/main/java/com/trendyol/transmission/effect/RouterPayloadEffect.kt
+++ transmission/src/main/java/com/trendyol/transmission/effect/RouterPayloadEffect.kt
@@ -1,5 +1,11 @@
 package com.trendyol.transmission.effect
 
 import com.trendyol.transmission.Transmission
+import com.trendyol.transmission.TransmissionRouter
+import com.trendyol.transmission.transformer.Transformer
 
+/**
+ * This effect does not get published to other [Transformer]s.
+ * Use it to send payload directly to [TransmissionRouter].
+ */
 data class RouterPayloadEffect(val payload: Any): Transmission.Effect
