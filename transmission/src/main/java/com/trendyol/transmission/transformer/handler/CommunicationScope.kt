c799bfc68e04053f411264cfdb66939e276e64e2
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-05-03 23:02:29 +0300

    Simplify CommunicationScope API, add documentation

--- transmission/src/main/java/com/trendyol/transmission/transformer/handler/CommunicationScope.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/handler/CommunicationScope.kt
@@ -1,6 +1,7 @@
 package com.trendyol.transmission.transformer.handler
 
 import com.trendyol.transmission.Transmission
+import com.trendyol.transmission.TransmissionRouter
 import com.trendyol.transmission.transformer.Transformer
 import com.trendyol.transmission.transformer.query.QuerySender
 import kotlin.reflect.KClass
@@ -8,7 +9,22 @@ import kotlin.reflect.KClass
 typealias usingCommunication = CommunicationScope<Transmission.Data, Transmission.Effect>
 
 interface CommunicationScope<D : Transmission.Data, E : Transmission.Effect>: QuerySender<D,E> {
-	fun publishData(data: D?)
-	fun publishEffect(effect: E)
-	fun sendEffect(effect: E, to: KClass<out Transformer<D, E>>)
+	/**
+	 * Sends data to [TransmissionRouter]
+	 * @param data of type [Transmission.Data]
+	 */
+	fun send(data: D?)
+
+	/**
+	 * Publishes [Transmission.Effect] to other [Transformer]s
+	 * @param effect of type [Transmission.Effect]
+	 */
+	fun publish(effect: E)
+
+	/**
+	 * Sends [Transmission.Effect] to a specific [Transformer]
+	 * @param effect of type [Transmission.Effect]
+	 * @param to Target [Transformer]
+	 */
+	fun send(effect: E, to: KClass<out Transformer<D, E>>)
 }
