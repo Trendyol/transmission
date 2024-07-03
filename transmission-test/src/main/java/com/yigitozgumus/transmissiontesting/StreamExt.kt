1cf98bac3c9c76d9aee1632b961ca488331e8f27
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-07-03 20:19:44 +0300

    Fix instance method extension casting

--- transmission-test/src/main/java/com/yigitozgumus/transmissiontesting/StreamExt.kt
+++ transmission-test/src/main/java/com/yigitozgumus/transmissiontesting/StreamExt.kt
@@ -2,10 +2,10 @@ package com.yigitozgumus.transmissiontesting
 
 import com.trendyol.transmission.Transmission
 
-inline fun<reified T: Transmission.Data?> List<Transmission.Data>.lastInstanceOf(): T? {
-    return this.filterIsInstance<T>().last()
+inline fun<reified T: Transmission.Data?> List<Transmission.Data>.lastVersionOf(): T? {
+    return this.filterIsInstance<T>().takeIf { it.isNotEmpty() }?.last()
 }
 
-inline fun<reified T: Transmission.Effect?> List<Transmission.Effect>.lastInstanceOf(): T? {
-    return this.filterIsInstance<T>().last()
+inline fun<reified T: Transmission.Effect?> List<Transmission.Effect>.lastVersionOf(): T? {
+    return this.filterIsInstance<T>().takeIf { it.isNotEmpty() }?.last()
 }
