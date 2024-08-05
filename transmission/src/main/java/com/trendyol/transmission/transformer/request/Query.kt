cacd9294c21443cb5834179fbb313dfb76d5904c
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-08-05 10:32:48 +0300

    Make Query sealed interface

--- transmission/src/main/java/com/trendyol/transmission/transformer/request/Query.kt
+++ transmission/src/main/java/com/trendyol/transmission/transformer/request/Query.kt
@@ -1,33 +1,33 @@
 package com.trendyol.transmission.transformer.request
 
-internal sealed class Query {
+internal sealed interface Query {
 
     data class Data(
         val sender: String,
         val key: String,
-    ) : Query()
+    ) : Query
 
     data class Computation(
         val sender: String,
         val key: String,
         val invalidate: Boolean = false,
-    ) : Query()
+    ) : Query
 
     data class ComputationWithArgs<A : Any>(
         val sender: String,
         val key: String,
         val args: A,
         val invalidate: Boolean = false,
-    ) : Query()
+    ) : Query
 
     data class Execution(
         val sender: String,
         val key: String,
-    ) : Query()
+    ) : Query
 
     data class ExecutionWithArgs<A : Any>(
         val sender: String,
         val key: String,
         val args: A,
-    ) : Query()
+    ) : Query
 }
