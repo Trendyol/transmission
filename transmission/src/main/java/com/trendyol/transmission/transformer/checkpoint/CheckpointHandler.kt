da8ff8e04fc57b953d5b8875e7da28e2c654f3ab
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2024-09-22 23:59:44 +0300

    Add CheckpointHandler API methods

---
+++ transmission/src/main/java/com/trendyol/transmission/transformer/checkpoint/CheckpointHandler.kt
@@ -0,0 +1,32 @@
+package com.trendyol.transmission.transformer.checkpoint
+
+import com.trendyol.transmission.ExperimentalTransmissionApi
+import com.trendyol.transmission.transformer.handler.CommunicationScope
+import com.trendyol.transmission.transformer.request.Contract
+
+interface CheckpointHandler {
+
+    @ExperimentalTransmissionApi
+    suspend fun CommunicationScope.pauseOn(
+        contract: Contract.Checkpoint,
+        resumeBlock: suspend CommunicationScope.() -> Unit
+    )
+
+    @ExperimentalTransmissionApi
+    suspend fun CommunicationScope.pauseOn(
+        vararg contract: Contract.Checkpoint,
+        resumeBlock: suspend CommunicationScope.() -> Unit
+    )
+
+    @ExperimentalTransmissionApi
+    suspend fun <C : Contract.CheckpointWithArgs<A>, A : Any> CommunicationScope.pauseOn(
+        contract: C,
+        resumeBlock: suspend CommunicationScope.(args: A) -> Unit
+    )
+
+    @ExperimentalTransmissionApi
+    suspend fun validate(contract: Contract.Checkpoint)
+
+    @ExperimentalTransmissionApi
+    suspend fun <C : Contract.CheckpointWithArgs<A>, A : Any> validate(contract: C, args: A)
+}
