32d9c85626b81aba8e86c1057a13f96ab7711ae3
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-11-28 07:57:31 +0300

    Delete old testing code and bind query processing to actual one

--- transmission/src/main/java/com/trendyol/transmission/router/builder/TransmissionRouterBuilderInternal.kt
+++ transmission/src/main/java/com/trendyol/transmission/router/builder/TransmissionRouterBuilderInternal.kt
@@ -1,30 +1,22 @@
 package com.trendyol.transmission.router.builder
 
-import com.trendyol.transmission.router.RegistryScope
-import com.trendyol.transmission.router.RegistryScopeImpl
 import com.trendyol.transmission.router.loader.TransformerSetLoader
 import com.trendyol.transmission.transformer.Transformer
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 internal class TransmissionRouterBuilderInternal internal constructor(
-    scope: TransmissionTestingRouterBuilderScope.() -> Unit
+    scope: TransmissionRouterBuilderScope.() -> Unit
 ) {
 
     internal var dispatcher: CoroutineDispatcher = Dispatchers.Default
-    internal var registryScope: RegistryScopeImpl? = null
     internal lateinit var transformerSetLoader: TransformerSetLoader
     internal var autoInitialization: Boolean = true
 
-    private val scopeImpl = object : TransmissionTestingRouterBuilderScope {
-
-        override fun testing(scope: RegistryScope.() -> Unit) {
-            this@TransmissionRouterBuilderInternal.registryScope = RegistryScopeImpl().apply(scope)
-        }
+    private val scopeImpl = object : TransmissionRouterBuilderScope {
 
         override fun addDispatcher(dispatcher: CoroutineDispatcher) {
             this@TransmissionRouterBuilderInternal.dispatcher = dispatcher
-
         }
 
         override fun addTransformerSet(transformerSet: Set<Transformer>) {
