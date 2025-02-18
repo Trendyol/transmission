82a06dd6cd63c975affcd9ea79f72476b8ef8de0
Yigit Ozgumus <yigitozgumus1@gmail.com>
2025-02-18 13:59:28 +0300

    Convert Capacity to Value Class

--- transmission/src/main/java/com/trendyol/transmission/router/Capacity.kt
+++ transmission/src/main/java/com/trendyol/transmission/router/Capacity.kt
@@ -1,14 +1,13 @@
 package com.trendyol.transmission.router
 
-sealed class Capacity(val value: Int) {
+@JvmInline
+value class Capacity private constructor(val value: Int) {
+    companion object {
+        val Default = Capacity(64)
 
-    init {
-        check(value in 0..256) {
-            "bufferCapacity should be between 0 and 256"
+        fun Custom(value: Int): Capacity {
+            require(value in 0..256) { "bufferCapacity should be between 0 and 256" }
+            return Capacity(value)
         }
     }
-
-    data object Default : Capacity(64)
-
-    data class Custom(val customValue: Int) : Capacity(customValue)
 }
